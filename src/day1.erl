21641bd1e7d361d34f13144fa13bff78aaf4045d
Yatin Kanetkar <yatin.kanetkar@gmail.com>
2017-12-01 15:03:16 -0500

    Parts 1 and 2

--- src/day1.erl
+++ src/day1.erl
@@ -1,5 +1,5 @@
 -module(day1).
--export([calc_sum/1]).
+-export([calc_sum/2, part1/1, part2/1]).
 
 
 % aggregator(T, AccIn) -> AccOut
@@ -8,8 +8,11 @@ aggregator({X, X}, Acc) ->
 aggregator({_, _}, Acc) ->
     Acc.
 
-calc_sum(List) ->
-    {L, _} = lists:split(length(List) - 1, List),
-    Cycled = [lists:last(List) | L],
+calc_sum(List, CycleLength) ->
+    {L, R} = lists:split(length(List) - CycleLength, List),
+    Cycled = R ++ L,
     lists:foldl(fun aggregator/2, 0, lists:zip(List, Cycled)).
-    
+
+part1(List) -> calc_sum(List, 1).
+part2(List) -> calc_sum(List, length(List) div 2).
+
