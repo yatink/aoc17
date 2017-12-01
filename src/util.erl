81d651fe1fb9173a403956689d6c52fab99cebcb
Yatin Kanetkar <yatin.kanetkar@gmail.com>
2017-12-01 12:52:10 -0500

    Simplify the string to list of integers conversion

--- src/util.erl
+++ src/util.erl
@@ -1,16 +1,7 @@
 -module(util).
 -export([string_to_ints/1]).
 
-converter(48) -> 0;
-converter(49) -> 1;
-converter(50) -> 2;
-converter(51) -> 3;
-converter(52) -> 4;
-converter(53) -> 5;
-converter(54) -> 6;
-converter(55) -> 7;
-converter(56) -> 8;
-converter(57) -> 9.
+converter(X) when X >= 48, X =< 57 -> X - 48.
 
 string_to_ints(String) ->
     lists:map(fun converter/1, String).
