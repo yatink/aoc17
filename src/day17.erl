25dcd6749bfba232a31f6a8a0daf15adfc5f527c
Yatin <yatin>
2017-12-21 22:25:12 -0500

    Somewhat working day17 part 2. Using Qs is waaaaay faster

--- src/day17.erl
+++ src/day17.erl
@@ -1,5 +1,5 @@
 -module(day17).
--export([spinlock/5, find_elem/3, part1/2, part2/2]).
+-export([spinlock/5, find_elem/3, part1/2, part2/2, rotate/2, q_based_spinlock/5]).
 
 spinlock(Acc, Iters, CurrentPosition, StepSize, MaxSize) when Iters < MaxSize ->
     erlang:display(hd(Acc)),
@@ -10,19 +10,25 @@ spinlock(Acc, _, _, _, _) ->
 
 rotate(Q, 0) -> Q;
 rotate(Q, RotationDegrees) ->
-    rotate(queue:in(queue:out(Q), Q), RotationDegrees - 1).
+    {{value, Elem}, NewQ} = queue:out(Q),
+    rotate(queue:in(Elem, NewQ), RotationDegrees - 1).
 
 q_based_spinlock(Q, RotationDegrees, StepSize, CurrentIter, MaxIters) when CurrentIter < MaxIters ->
-    NewPosition = (RotationDegrees + StepSize) rem CurrentIter,
-    
+    NewPosition = StepSize rem CurrentIter,
+    %% erlang:display({queue:to_list(Q), RotationDegrees, CurrentIter, NewPosition}),
+    %% RotatedQ = rotate(Q, NewPosition),
+    q_based_spinlock(queue:in(CurrentIter, rotate(Q, NewPosition)), RotationDegrees + NewPosition, StepSize, CurrentIter + 1, MaxIters);
+q_based_spinlock(Q, RotationDegrees, _, _, _) ->
+    {Q, RotationDegrees}.
+
 
 find_elem([Elem|_], Elem, Index) -> Index;
 find_elem([_|T], Elem, Index) -> find_elem(T, Elem, Index + 1).
    
 
-part1(StepSize, Iters) ->
-    L = spinlock([0], 1, 0, StepSize, Iters),
+part1(StepSize, MaxIters) ->
+    L = spinlock([0], 1, 0, StepSize, MaxIters),
     lists:nth(find_elem(L, 2017, 1) + 1, L).
 
-part2(StepSize, Iters) ->
-    L = spinlock([0], 1, 0, StepSize, Iters).
+part2(StepSize, MaxIters) ->
+    q_based_spinlock(queue:from_list([0]), 0, StepSize, 1, MaxIters).
