8767b8664c9b68ff2831693e6847bf1fa1c09b29
Yatin <yatin>
2017-12-25 17:05:03 -0500

    Still haven't solved it :-(. Infinite loop

--- src/day18.erl
+++ src/day18.erl
@@ -1,5 +1,5 @@
 -module(day18).
--export([get_integer_or_register/2, parse_instruction/3, parse_instruction_set/4, part1/1, supervisor/3, child/3, part2/1]).
+-export([get_integer_or_register/2, parse_instruction/3, parse_instruction_set/6, part1/1, child/3, part2/1, run_part2/0, run_part2_simple/0]).
 
 get_integer_or_register(StrValue, Registers) ->
     try list_to_integer(StrValue) of
@@ -13,72 +13,110 @@ parse_instruction([Instr|Args], Registers, InstrModule) ->
                  [Register, StrValue] -> 
                      Value = get_integer_or_register(StrValue, Registers),
                      [Register, Value, Registers];
-                 [Register] -> [Register, Registers]
+                 [StrValue] -> 
+		     [StrValue, Registers] 
              end,
-    erlang:display({[Instr|Args], Registers}),
     apply(list_to_atom(InstrModule), list_to_atom(Instr), FnArgs).
+		
 
-parse_instruction_set(InstructionSet, InstrPtr, Registers, InstrModule) ->
-    erlang:display({InstructionSet, InstrPtr}),
+parse_instruction_set(InstructionSet, InstructionSetSize, InstrPtr, Registers, InstrModule, SendCount) when InstrPtr < InstructionSetSize->
     case parse_instruction(string:tokens(array:get(InstrPtr, InstructionSet), " "), Registers, InstrModule) of
-	wait -> {InstrPtr + 1, Registers};
-        {Freq, done} -> Freq;
+	deadlock -> {deadlock, SendCount};
+        {Freq, done} -> {complete_part1, Freq};
         {NewState, Offset} -> 
-	    parse_instruction_set(InstructionSet, InstrPtr + Offset, NewState, InstrModule)
-    end.
+	    erlang:display("Nothing here"),
+	    parse_instruction_set(InstructionSet, InstructionSetSize, InstrPtr + Offset, NewState, InstrModule, SendCount);
+	{NewState, Offset, SendCountOffset} -> 
+	    parse_instruction_set(InstructionSet, InstructionSetSize, InstrPtr + Offset, NewState, InstrModule, SendCount + SendCountOffset)
+    end;
+parse_instruction_set(_, _, _, _, _, SendCount) -> {finished_instr, SendCount}.
+
             
 part1(InstructionStrings) ->
     {Instructions, _} = lists:foldl(
                           fun(Instr, {InstrSet, Index}) -> {array:set(Index, Instr, InstrSet), Index + 1} end,
                           {array:new(), 0},
                           InstructionStrings),
-    parse_instruction_set(Instructions, 0, maps:new(), "day18part1").
+    parse_instruction_set(Instructions, array:size(Instructions), 0, maps:new(), "day18part1", 0).
                                        
-supervisor(ChildrenPids, BlockedCount, SendCount) when BlockedCount < 2->
-    lists:foreach(fun(Pid) -> Pid ! {start, self()} end, maps:values(ChildrenPids)),
-    receive 
-        {snd_request, Value, From} ->
-            To = case From of
-                     0 -> 1;
-                     1 -> 0
-                 end,
-            maps:get(To, ChildrenPids) ! {send, Value},
-            supervisor(ChildrenPids, BlockedCount, maps:put(From, maps:get(From, SendCount, 0) + 1, SendCount));
-        {rcv_request, Child} ->
-            maps:get(Child, ChildrenPids) ! {check_blocked},
-            supervisor(ChildrenPids, BlockedCount, SendCount);
-        blocked ->
-	    erlang:display("Blocked"),
-            supervisor(ChildrenPids, BlockedCount + 1, SendCount);
-        not_blocked ->
-	    erlang:display("Unblocked"),
-            supervisor(ChildrenPids, BlockedCount - 1, SendCount)
-    end;
-supervisor(_, _, SendCount) -> 
-    erlang:display(SendCount),
-    SendCount.
 
 child(Registers, InstructionSet, InstrPtr) ->
     receive 
-	{start, Pid} -> 
-	    {NewPtr, NewState} = parse_instruction_set(InstructionSet, InstrPtr, maps:put(supervisor, Pid, Registers), "day18part2"),
-	    maps:get(supervisor, Registers) ! blocked,
-	    child(NewState, InstructionSet, NewPtr);
-	{send, Value} ->
-	    maps:get(supervisor, Registers) ! not_blocked,
-	    [_, Register] = string:tokens(array:get(InstrPtr, InstructionSet), " "),
-	    {NewPtr,NewState} = parse_instruction_set(InstructionSet, InstrPtr + 1, maps:put(Register, Value, Registers), "day18part2"),
-	    maps:get(supervisor, Registers) ! blocked,
-	    child(NewState, InstructionSet, NewPtr)
+	{_, {start, PeerPid}} -> 
+	    Ret = parse_instruction_set(InstructionSet, array:size(InstructionSet), InstrPtr, maps:put(peer, PeerPid, Registers), "day18part2", 0),
+	    erlang:display({start, Ret, InstrPtr, Registers})
     end.
+
+start_processing(Pid, PeerPid) ->
+    Pid ! {self(), {start, PeerPid}}.
+				        
+create(Id, Instructions) ->
+    spawn(day18, child, [#{"p" => Id, self => Id}, Instructions, 0]).
 
 part2(InstructionStrings) ->
     {Instructions, _} = lists:foldl(
                           fun(Instr, {InstrSet, Index}) -> {array:set(Index, Instr, InstrSet), Index + 1} end,
                           {array:new(), 0},
                           InstructionStrings),
-    Child0 = spawn(day18, child, [#{"p" => 0}, Instructions, 0]),
-    Child1 = spawn(day18, child, [#{"p" => 1}, Instructions, 0]),
-    SuperVisorPid = spawn(day18, supervisor, [#{0 => Child0, 1 => Child1}, 0, #{0 => 0, 1 => 0}]),
-    lists:foreach(fun(Pid) -> Pid ! {update_supervisor, SuperVisorPid} end, [Child0, Child1]),
-    lists:foreach(fun(Pid) -> Pid ! {start} end, [Child0, Child1]).
+    %% erlang:display("Spawning Children"),
+    Child0 = create(0, Instructions),
+    Child1 = create(1, Instructions),
+
+    start_processing(Child0, Child1),
+    start_processing(Child1, Child0).
+
+run_part2() ->
+    Ip = string:tokens(
+	   "set i 31
+set a 1
+mul p 17
+jgz p p
+mul a 2
+add i -1
+jgz i -2
+add a -1
+set i 127
+set p 826
+mul p 8505
+mod p a
+mul p 129749
+add p 12345
+mod p a
+set b p
+mod b 10000
+snd b
+add i -1
+jgz i -9
+jgz a 3
+rcv b
+jgz b -1
+set f 0
+set i 126
+rcv a
+rcv b
+set p a
+mul p -1
+add p b
+jgz p 4
+snd a
+set a b
+jgz 1 3
+snd b
+set f 1
+add i -1
+jgz i -11
+snd a
+jgz f -16
+jgz a -19", "\n"), 
+    part2(Ip).
+
+run_part2_simple() ->
+    Ip = string:tokens(
+	   "snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d", "\n"),
+    part2(Ip).
